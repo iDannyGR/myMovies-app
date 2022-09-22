06994fd6c62dd3307ad9d0a863b739f7770eee45
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-09-22 12:03:00 -0600

    [add movie card] 22/09/022

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './assets/sass/global.scss'
 import Login from './pages/Login'
+import { MovieInfo } from 'components/MovieInfo';
 
 const App = ()=> {
   return (
     <div className="App">
      <Login/>
+     <MovieInfo />
     </div>
   );
 }
